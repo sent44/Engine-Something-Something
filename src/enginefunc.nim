daf4475b1cbb3d65ff4ddd8bec65a5ace317cc4b
sent44 <sentbysent@gmail.com>
2022-04-11 04:56:36 +0700

    BitArray with bug gone!

--- src/enginefunc.nim
+++ src/enginefunc.nim
@@ -2,6 +2,9 @@
 import std/math
 
 
-
 func `==?`*[T: SomeFloat](a, b: T): bool = a.almostEqual(b)
 template `!=?`*[T: SomeFloat](a, b: T): bool = not a ==? b
+
+iterator items*(n: int): int =
+    for i in 0 ..< n:
+        yield i
--- src/types/bitarray.nim
+++ src/types/bitarray.nim
@@ -12,24 +12,39 @@ type BitArray*[N: static[int]] = distinct array[div8 N, uint8]
 func len*[N](arr: BitArray[N]): int = N
 
 func `[]`*[N](arr: BitArray[N], i: int): bool =
-    return array[div8 N, uint8](arr)[div8 i].testBit (i mod 8).uint8
-proc `[]=`*[N](arr: var BitArray[N], i: int, value: bool): void =
+    return array[div8 N, uint8](arr)[int i/8].testBit (i mod 8).uint8
+func `[]=`*[N](arr: var BitArray[N], i: int, value: bool): void =
     if value:
-        array[div8 N, uint8](arr)[div8 i].setBit (i mod 8).uint8
-        # arr.array[div8 i].setBit (i mod 8).uint8
+        array[div8 N, uint8](arr)[int i/8].setBit (i mod 8).uint8
     else:
-        array[div8 N, uint8](arr)[div8 i].clearBit (i mod 8).uint8
-        # arr.array[div8 i].clearBit (i mod 8).uint8
+        array[div8 N, uint8](arr)[int i/8].clearBit (i mod 8).uint8
 
-# var test: BitArray[9]
-# echo test[3]
-# test[3] = true
-# echo test[3]
+func allTrue*(arr: var BitArray): void =
+    for i in arr.len:
+        arr[i] = true
+func allFalse*[N](arr: var BitArray[N]): void =
+    var a: BitArray[N]
+    arr = a
 
-# for i in test:
-#     echo test[i]
+iterator items*[N](arr: BitArray[N]): bool =
+    for i in arr.len:
+        yield arr[i]
 
-# func newBitArray[N: static[int]](): BitArray[N] =
+# converter toBitArray*[N: static[int]](arr: array[N, bool]): BitArray[N] =
+#     for i in N:
+#         result[i] = arr[i]
+func toBitArray*[N: static[int]](arr: array[N, bool]): BitArray[N] =
+    for i in N:
+        result[i] = arr[i]
 
+func toString(arr: BitArray): string =
+    result = "́[" # Hidden acute accent here!
+    for i in arr.len:
+        result &= $arr[i]
+        if i < arr.len - 1:
+            result &= ", "
+    result &= "]"
+func `$`(arr: BitArray): string = arr.toString()
 
-# echo newBitArray[17]()
+echo @[1, 2, 3]
+echo [false, true, true].toBitArray
